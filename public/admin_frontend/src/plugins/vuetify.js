b96d5f07d6409ce5db223afa4a050f00f9987831
Henrique de Oliveira <henriqueoliwork@gmail.com>
2024-01-25 15:55:33 -0300

    feat: new login page design

--- public/admin_frontend/src/plugins/vuetify.js
+++ public/admin_frontend/src/plugins/vuetify.js
@@ -4,4 +4,16 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                // vuetify colors
+                primary: '#7069A1',
+
+                // custom colors
+                galaxy: '#7069A1',
+                stardust: '#0F151C'
+            }
+        }
+    }
 });
--- public/admin_frontend/src/views/Login.vue
+++ public/admin_frontend/src/views/Login.vue
@@ -1,39 +1,88 @@
 <template>
     <div class="login_container">
-        <v-img contain class="logo" src="@/assets/logo.png"></v-img>
-        <v-card class="login_card" elevation="2">
-            <v-card-title>Entrar</v-card-title>
-            <v-card-subtitle>Entre com suas informações de login</v-card-subtitle>
+        <v-img src="/login-background-pattern.png" class="login_background_pattern"></v-img>
 
-            <v-card-text>
-                <v-row>
-                    <v-col cols="12">
-                        <v-text-field v-model="email" label="Email" outlined hide-details="auto"></v-text-field>
+        <v-container>
+
+            <v-card
+                width="100%"
+                max-width="1180"
+                class="overflow-hidden mx-auto"
+            >      
+            
+                <v-row no-gutters>
+                    <v-col
+                        cols="6"
+                        class="d-none d-sm-flex"
+                    >
+                        <v-img cover height="100%" max-height="580" src="/login-image.png"></v-img>
                     </v-col>
-                    <v-col cols="12">
-                        <v-text-field v-model="password" type="password" label="Senha" outlined hide-details="auto"></v-text-field>
+        
+                    <v-col class="d-flex align-center justify-center" cols="12" sm="6">
+                        <v-form @submit.prevent="login" >
+                            
+                            <v-card tile flat>
+                                <v-card-title class="primary--text font-weight-bold font-ubuntu">  
+                                    Bem-vindo de volta!
+                                </v-card-title>
+                                
+                                <v-card-subtitle class="stardust--text font-weight-bold font-ubuntu"> 
+                                    Faça seu login abaixo.
+                                </v-card-subtitle>
+        
+                                <v-card-text>
+                                    <v-row>
+                                        <v-col cols="12">
+                                            <v-text-field
+                                                v-model="email"
+                                                label="Email"
+                                                outlined
+                                                hide-details="auto"
+                                            />
+                                        </v-col>
+                                        <v-col cols="12">
+                                            <v-text-field
+                                                v-model="password"
+                                                type="password"
+                                                label="Senha"
+                                                outlined
+                                                hide-details="auto"
+                                            />
+        
+                                            <div class="mt-4 d-flex">
+                                                <a href="#" class="stardust--text font-weight-bold font-ubuntu ml-auto text-decoration-none text-caption">
+                                                    Esqueceu sua senha?
+                                                </a>
+                                            </div>
+                                        </v-col>
+                                        <v-col cols="12">
+                                            <v-btn
+                                                depressed
+                                                large
+                                                color="primary"
+                                                width="100%"
+                                                type="submit"
+                                            >
+                                                Entrar
+                                            </v-btn>
+                                        </v-col>
+                                    </v-row>
+                                </v-card-text>
+                            </v-card>
+                        </v-form>
                     </v-col>
                 </v-row>
-            </v-card-text>
-
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn depressed large color="primary" @click.native="login">
-                    Entrar
-                </v-btn>
-            </v-card-actions>
-        </v-card>
+            </v-card>
+        </v-container>
     </div>
 </template>
   
 <script>
 import Api from "@/lib/Api";
 import { emitToastr } from "@/lib/Utils";
-//import { emitToastr } from "@/lib/Utils";
 
 export default {
     name: "LoginView",
-
     data() {
         return {
             email: "",
@@ -41,16 +90,10 @@ export default {
             remember: true,
         };
     },
-
-    components: {},
-
-    mounted() {
-        
-    },
-
     methods: {
         async login() {
             const resp = await Api.login(this.email, this.password, this.remember);
+
             if (resp && resp.login_ok) {
                 this.$router.push("/");
                 return;
@@ -67,25 +110,21 @@ export default {
   
 <style scoped>
 .login_container {
+    background: linear-gradient(151.26deg, #615B95 0%, #29273D 58.5%);
     display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    height: 100%;
-    align-content: center;
     align-items: center;
+    justify-content: center;
+    height: 100vh;
 }
 
-.logo {
-    max-width: 300px;
-    max-height: 300px;
+.login_background_pattern {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
 }
 
-@media only screen and (max-width: 768px) {
-    .login_container {
-        text-align: center;
-        padding: 8%;
-    }
 
-}
 </style>
   
