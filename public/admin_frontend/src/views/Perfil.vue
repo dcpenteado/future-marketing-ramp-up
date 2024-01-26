b9e1475e0cb0a9e5d77556f5c6e648115cb89b2a
Henrique de Oliveira <henriqueoliwork@gmail.com>
2024-01-26 17:36:30 -0300

    feat: start user profile page

--- public/admin_frontend/src/views/Perfil.vue
+++ public/admin_frontend/src/views/Perfil.vue
@@ -1,11 +1,57 @@
 <template>
-    <div class="menu-page">
-        <h1>Meu Perfil</h1>
+    <div>
+        <v-card outlined>
+            <v-card-text>
+                <v-form>
+                    <v-row>
+                        <v-col cols="12" md="2" class="d-flex justify-center">
+                            <v-avatar size="160">
+                                <div class="w-full h-full primary d-flex justify-center">
+                                    <v-icon color="white" style="font-size:120px" >mdi-account</v-icon>
+                                </div>
+                            </v-avatar>
+                        </v-col>
+
+                        <v-col cols="12" md="10">
+                            <v-row>
+                                <v-col cols="12">
+                                    <v-text-field
+                                        v-model="form.name"
+                                        label="Nome"
+                                        required
+                                        outlined
+                                        hide-details="auto"
+                                    />    
+                                </v-col>
+                                
+                                <v-col cols="12">
+                                    <v-text-field
+                                        v-model="form.email"
+                                        label="E-mail"
+                                        required
+                                        outlined
+                                        hide-details="auto"
+                                    />    
+                                </v-col>
+                            </v-row>
+                        </v-col>
+
+                        
+                        <v-col cols="12" class="d-flex">
+                            <v-spacer />
+                            <v-btn color="error" class="mr-2">Cancel</v-btn>
+                            <v-btn color="primary">Salvar</v-btn>
+                        </v-col>
+                    </v-row> 
+                </v-form>
+            </v-card-text>
+        </v-card>
+        
     </div>
 </template>
   
 <script>
-//import Api from "@/lib/Api";
+import Api from "@/lib/Api";
 //import { emitToastr } from "@/lib/Utils";
 
 export default {
@@ -13,11 +59,18 @@ export default {
 
     data() {
         return {
+            form: {
+                name: '',
+                email: '',
+            }
         };
     },
+    mounted(){
+        const user = Api.getUser()
 
-    components: {},
-
+        this.form.name = user.name
+        this.form.email = user.email
+    },
     methods: {
     },
 };
