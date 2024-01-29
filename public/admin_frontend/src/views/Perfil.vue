a6aa1425b159521e554990038985e8348c387620
Henrique de Oliveira <henriqueoliwork@gmail.com>
2024-01-29 14:56:28 -0300

    refactor: add image upload functionality on profile page

--- public/admin_frontend/src/views/Perfil.vue
+++ public/admin_frontend/src/views/Perfil.vue
@@ -2,45 +2,56 @@
     <div>
         <v-card outlined>
             <v-card-text>
-                <v-form>
+                <v-form @submit.prevent="save">
                     <v-row>
-                        <v-col cols="12" md="2" class="d-flex justify-center">
-                            <v-avatar size="160">
-                                <div class="w-full h-full primary d-flex justify-center">
-                                    <v-icon color="white" style="font-size:120px" >mdi-account</v-icon>
-                                </div>
-                            </v-avatar>
-                        </v-col>
-
-                        <v-col cols="12" md="10">
+                        <v-col cols="12">
                             <v-row>
-                                <v-col cols="12">
-                                    <v-text-field
-                                        v-model="form.name"
-                                        label="Nome"
-                                        required
-                                        outlined
-                                        hide-details="auto"
-                                    />    
+                                <v-col cols="12" sm="4" md="3" lg="2" class="d-flex justify-center">
+                                    <v-avatar size="160">
+                                        <v-img :src="image.preview" v-if="image.preview" />
+                                        <div v-else class="w-full h-full primary d-flex justify-center">
+                                            <v-icon color="white" style="font-size:120px" >mdi-account</v-icon>
+                                        </div>
+                                    </v-avatar>
                                 </v-col>
-                                
-                                <v-col cols="12">
-                                    <v-text-field
-                                        v-model="form.email"
-                                        label="E-mail"
-                                        required
-                                        outlined
-                                        hide-details="auto"
-                                    />    
+
+                                <v-col cols="12" sm="auto" class="d-flex align-center text-center text-sm-left">
+                                    <div>
+                                        <v-btn color="primary" class="mb-2 mb-sm-0 mr-sm-2" @click="pickFile">Alterar Foto</v-btn>
+                                        <v-btn color="error" @click="removeImage" >Remover Foto</v-btn>
+    
+                                        <div class="w-full mt-4">
+                                            Imagens permitidas: JPG, PNG e com tamanho máximo de 2MB.
+                                        </div>
+                                    </div>
                                 </v-col>
                             </v-row>
                         </v-col>
 
+                        <v-col cols="12" md="6">
+                            <v-text-field
+                                v-model="form.name"
+                                label="Nome"
+                                required
+                                outlined
+                                hide-details="auto"
+                            />    
+                        </v-col>
+                        
+                        <v-col cols="12" md="6"> 
+                            <v-text-field
+                                v-model="form.email"
+                                label="E-mail"
+                                required
+                                outlined
+                                hide-details="auto"
+                            />    
+                        </v-col>
                         
                         <v-col cols="12" class="d-flex">
                             <v-spacer />
-                            <v-btn color="error" class="mr-2">Cancel</v-btn>
-                            <v-btn color="primary">Salvar</v-btn>
+                            <v-btn color="error" class="mr-2" @click="load">Cancel</v-btn>
+                            <v-btn color="primary" type="submit">Salvar</v-btn>
                         </v-col>
                     </v-row> 
                 </v-form>
@@ -52,7 +63,7 @@
   
 <script>
 import Api from "@/lib/Api";
-//import { emitToastr } from "@/lib/Utils";
+import { emitToastr } from "@/lib/Utils";
 
 export default {
     name: "PerfilView",
@@ -62,16 +73,50 @@ export default {
             form: {
                 name: '',
                 email: '',
+            },
+            image: {
+                file: null,
+                preview: null,
             }
         };
     },
     mounted(){
-        const user = Api.getUser()
-
-        this.form.name = user.name
-        this.form.email = user.email
+        this.load()
     },
     methods: {
+        load(){
+            const user = Api.getUser()
+
+            this.form.name = user.name
+            this.form.email = user.email
+        },
+        pickFile(){
+            const input = document.createElement('input')
+
+            input.type = 'file'
+            input.accept = 'image/jpeg, image/png'
+
+            input.onchange = (e) => {
+                const file = e.target.files[0]
+
+                this.setImage(file)
+            }
+            
+            input.click()
+        },
+        removeImage(){
+            this.image.file = null
+            this.image.preview = null
+        },
+        setImage(file){
+            const preview = URL.createObjectURL(file)
+
+            this.image.file = file
+            this.image.preview = preview
+        },
+        save(){
+            emitToastr('error', 'Não implementado')
+        }
     },
 };
 </script>
